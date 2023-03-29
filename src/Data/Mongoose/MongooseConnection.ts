9c2bf7126ea8ca1d09883de2eb2afa67e07a8089
AmirHosein <awir.software@gmail.com>
2023-03-29 19:13:01 +0430

    set config

--- src/Data/Mongoose/MongooseConnection.ts
+++ src/Data/Mongoose/MongooseConnection.ts
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
 import IDatabaseService from "../interfaces/Repository/IDatabaseService";
+import  config  from "config";
 
 export default class MongooseConnection implements IDatabaseService{
     async connect(): Promise<void> {
         // connect to mongodb
-        await mongoose.connect('mongodb://127.0.0.1/Express-Type');
+        await mongoose.connect(config.get('db'));
     }
 }
