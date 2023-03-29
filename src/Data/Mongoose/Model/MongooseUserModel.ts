efcf478fc15847aac90afcb310fe56cc0b8a42f1
AmirHosein <awir.software@gmail.com>
2023-03-29 16:22:00 +0430

    set user mongoose model

--- src/Data/Mongoose/Model/MongooseUserModel.ts
+++ src/Data/Mongoose/Model/MongooseUserModel.ts
@@ -1,2 +1,19 @@
+import mongoose, { model } from "mongoose";
+import User from "../../Entities/User";
 
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        trim: true
+    },
+    number: {
+        type: String,
+        trim: true
+    },
+    email: {
+        type: String,
+        trim: true
+    }
+})
 
+export default model<User>('users', userSchema);
