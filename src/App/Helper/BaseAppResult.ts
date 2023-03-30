52c890e428aa4ca8ae531fc871d178799c20de84
AmirHosein <awir.software@gmail.com>
2023-03-31 02:56:51 +0430

    set app result

--- src/App/Helper/BaseAppResult.ts
+++ src/App/Helper/BaseAppResult.ts
@@ -1,1 +1,10 @@
 import { ResultStatus } from "./ResultStatus";
+
+export default class BaseAppResult<T>{
+    constructor(
+        public data: T,
+        public isError: boolean,
+        public status: ResultStatus,
+        public message: string
+    ){}
+}
