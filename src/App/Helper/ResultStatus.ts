4c4a410b6a1f2df61c19175533ebd22d3ab5ff65
AmirHosein <awir.software@gmail.com>
2023-03-31 02:53:57 +0430

    set data status

---
+++ src/App/Helper/ResultStatus.ts
@@ -0,0 +1,9 @@
+export enum ResultStatus {
+    Success = "Success",
+    Duplicate = "Duplicate",
+    Unknown = "Unknown",
+    NotMatch = "NotMatch",
+    Invalid = "Invalid",
+    NotFound = "NotFound",
+    NotAuthorized = "NotAuthorized",
+}
