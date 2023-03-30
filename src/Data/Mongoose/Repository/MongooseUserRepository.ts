8c6c38a5d52d222ad6f0b384e136b92c42953466
AmirHosein <awir.software@gmail.com>
2023-03-30 16:04:33 +0430

    set mongoose user

--- src/Data/Mongoose/Repository/MongooseUserRepository.ts
+++ src/Data/Mongoose/Repository/MongooseUserRepository.ts
@@ -1,13 +1,28 @@
 import User from "../../Entities/User";
 import IUser from "../../interfaces/IUser";
+import BaseDataError from "../../Model/BaseDataError";
 import BaseDataResult from "../../Model/BaseDataResult";
+import MongooseUserModel from "../Model/MongooseUserModel";
 
 export default class MongooseUserRepository implements IUser{
-    create(user: User): Promise<BaseDataResult<User>> {
-        
+    async create(user: User): Promise<BaseDataResult<User>> {
+        try {
+            const newUser = new MongooseUserModel(user);
+            const result = await newUser.save();
+            return new BaseDataResult<User>(result.toObject(), false);
+        } catch (error) {
+            throw new BaseDataError("Error While creating User", error);
+        }
     }
 
-    findByEmail(email: String): Promise<BaseDataResult<User>> {
-        
+    async findByEmail(email: String): Promise<BaseDataResult<User>> {
+        try {
+            const findUser = await MongooseUserModel.findOne({email: email});
+            if(!findUser) return new BaseDataResult<User>(null, true);
+
+            return new BaseDataResult<User>(findUser, false)
+        } catch (error) {
+            throw new BaseDataError("Error While finding User", error);
+        }
     }
 }
