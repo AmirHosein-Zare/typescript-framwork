bfd9d8a454d73e57468a871c9bd4e971838fbdb8
AmirHosein <awir.software@gmail.com>
2023-04-02 17:05:08 +0430

    set hash password

--- src/App/Service/PasswordService/PasswordService.ts
+++ src/App/Service/PasswordService/PasswordService.ts
@@ -1,11 +1,15 @@
 import IPasswordService from "../../interface/IPasswordService";
+import bcrypt from "bcrypt";
+import BaseDataError from "../../../Data/Helper/BaseDataError";
 
 export default class PasswordService implements IPasswordService {
     async hash(password: string): Promise<string> {
         try {
-            
+            return await bcrypt.hash(password, 15);
         } catch (error) {
-            
+            throw new BaseDataError("error while hashing password", error);
         }
     }   
+
+    
 }
