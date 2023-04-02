84a269b0f95000aea6c9567c409b9fa5d573c238
AmirHosein <awir.software@gmail.com>
2023-04-02 16:12:54 +0430

    add token service

---
+++ src/App/Service/TokenService/TokenService.ts
@@ -0,0 +1,11 @@
+import { ITokenService } from "../../interface/ITokenService"
+
+export default class TokenService implements ITokenService {
+    createToken(payload: any, secretKey: string, lifetime: number): Promise<string> {
+        
+    }
+
+    verifyToken<T>(token: string, secretKey: string): Promise<boolean | T> {
+        
+    }
+}
