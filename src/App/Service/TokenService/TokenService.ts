e1c28475ee1d2b25a53ffbd7b906bbefd995ca07
AmirHosein <awir.software@gmail.com>
2023-04-02 16:26:28 +0430

    set create token serivce

--- src/App/Service/TokenService/TokenService.ts
+++ src/App/Service/TokenService/TokenService.ts
@@ -1,9 +1,14 @@
 import { ITokenService } from "../../interface/ITokenService"
 import jwt from "jsonwebtoken"
+import BaseAppError from "../../Helper/BaseAppError";
 
 export default class TokenService implements ITokenService {
     async createToken(payload: any, secretKey: string, lifetime: number): Promise<string> {
-        
+        try {
+            return await jwt.sign(payload, secretKey, {expiresIn: lifetime});
+        } catch (error) {
+            throw new BaseAppError("error in create token", error);
+        }
     }
 
     async verifyToken<T>(token: string, secretKey: string): Promise<boolean | T> {
