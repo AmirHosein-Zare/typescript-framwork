b0c1438b322cfbaebff79fbc44bf1d780bfde48c
AmirHosein <awir.software@gmail.com>
2023-04-02 17:02:33 +0430

    add paswword service hash

---
+++ src/App/Service/PasswordService/PasswordService.ts
@@ -0,0 +1,11 @@
+import IPasswordService from "../../interface/IPasswordService";
+
+export default class PasswordService implements IPasswordService {
+    async hash(password: string): Promise<string> {
+        try {
+            
+        } catch (error) {
+            
+        }
+    }   
+}
