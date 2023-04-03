31fa8c69d4222b8afa08488e9b10cd1c476cb096
AmirHosein <awir.software@gmail.com>
2023-04-03 17:13:21 +0430

    add uuid symbol

--- src/App/Types/DataTypes.ts
+++ src/App/Types/DataTypes.ts
@@ -1,4 +1,5 @@
 export const AppDataTypes = {
     IPasswordService: Symbol.for('IPasswordService'),
-    ITokenService: Symbol.for('ITokenService')
+    ITokenService: Symbol.for('ITokenService'),
+    IDService: Symbol.for('IDService')
 }
