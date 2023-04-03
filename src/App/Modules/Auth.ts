44d9c51509f3fb15a975f8886374a24fb37f2f2e
AmirHosein <awir.software@gmail.com>
2023-04-03 17:48:57 +0430

    set get  token by email and password

--- src/App/Modules/Auth.ts
+++ src/App/Modules/Auth.ts
@@ -8,13 +8,16 @@ import { ResultStatus } from "../Helper/ResultStatus";
 import { AppDataTypes } from "../Types/DataTypes";
 import IPasswordService from "../interface/IPasswordService";
 import IIDService from "../interface/IIDService";
+import { ITokenService } from "../interface/ITokenService";
+import config from "config";
 
 @injectable()
 export default class Auth{
     constructor(
         @inject(DataTypes.IUser) private _user : IUser,
         @inject(AppDataTypes.IPasswordService) private _passwordService: IPasswordService,
-        @inject(AppDataTypes.IDService) private _idService: IIDService
+        @inject(AppDataTypes.IDService) private _idService: IIDService,
+        @inject(AppDataTypes.ITokenService) private _tokenService: ITokenService
     ){} 
 
     async createUser(name: string, email: string, password: string, number: string): Promise<BaseAppResult<User | {id: String}>>{
@@ -44,6 +47,55 @@ export default class Auth{
             );
         } catch (error) {
             throw new BaseAppError("error while create user", error);
+        }
+    }
+
+    async getTokenByEmailAndPassword(email: string, password: string): Promise<BaseAppResult<null | {token: string, lifetime: number}>>{
+        try {
+            const findUser = await this._user.findByEmail(email);
+
+            if(findUser.isError){
+                return new BaseAppResult<null | {token: string, lifetime: number}>(
+                    null,
+                    true,
+                    ResultStatus.Invalid,
+                    "Error while finding user by email"
+                )
+            }
+
+            if(!findUser.data){
+                return new BaseAppResult<null | {token: string, lifetime: number}>(
+                    null,
+                    true,
+                    ResultStatus.NotFound,
+                    "User not found"
+                )
+            }
+
+            const passwordCheck = await this._passwordService.verify(password, findUser.data.password.toString());
+
+            if(!passwordCheck){
+                return new BaseAppResult<null | {token: string, lifetime: number}>(
+                    null,
+                    true,
+                    ResultStatus.NotMatch,
+                    "password not Matched"
+                )
+            }
+
+            const generatedToken = await this._tokenService.createToken({email: findUser.data.email, password: findUser.data.password}, config.get('privateKey'), 86400);
+
+            return new BaseAppResult<{token: string, lifetime: number} | null>(
+                {
+                    token: generatedToken,
+                    lifetime: 86400
+                },
+                false,
+                ResultStatus.Success,
+                "Generate token success"
+            )
+        } catch (error) {
+            return new BaseAppResult<{ token: string; lifetime: number } | null>(null, true, ResultStatus.Unknown, "Error creating user.");
         }
     }
 
