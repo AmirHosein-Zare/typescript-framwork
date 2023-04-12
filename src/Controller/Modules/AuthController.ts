190de5c0df3026846e084a0bf6e7af305928ce7e
AmirHosein <awir.software@gmail.com>
2023-04-12 16:55:44 +0430

    add user mothod

--- src/Controller/Modules/AuthController.ts
+++ src/Controller/Modules/AuthController.ts
@@ -1,5 +1,7 @@
+import { NextFunction, Request, Response } from "express";
 import Auth from "../../App/Modules/Auth";
 import Controller from "../Controller";
+import { ResultStatus } from "../../App/Helper/ResultStatus";
 
 export default class AuthController extends Controller{
 
@@ -10,5 +12,10 @@ export default class AuthController extends Controller{
         super("/api");
     }
 
-    
+    async addUser(req: Request, res: Response, next: NextFunction): Promise<void>{
+        const result = await this._auth.createUser(req.body.name, req.body.email, req.body.password, req.body.number);
+        if(result.status !== ResultStatus.Success){
+            
+        }
+    }
 }
