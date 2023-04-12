f8519cece163a8e4b12f372d4d1fb846565c0991
AmirHosein <awir.software@gmail.com>
2023-04-12 17:02:43 +0430

    set auth controller function

--- src/Controller/Modules/AuthController.ts
+++ src/Controller/Modules/AuthController.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from "express";
 import Auth from "../../App/Modules/Auth";
 import Controller from "../Controller";
 import { ResultStatus } from "../../App/Helper/ResultStatus";
+import baseResponse from "../../Helper/BaseResponse";
 
 export default class AuthController extends Controller{
 
@@ -12,10 +13,15 @@ export default class AuthController extends Controller{
         super("/api");
     }
 
-    async addUser(req: Request, res: Response, next: NextFunction): Promise<void>{
+    async addUser(req: Request, res: Response, next: NextFunction){
         const result = await this._auth.createUser(req.body.name, req.body.email, req.body.password, req.body.number);
         if(result.status !== ResultStatus.Success){
-            
+            if (result.status === ResultStatus.Duplicate) {
+                return baseResponse(res, null, "already exist", undefined, ResultStatus.Duplicate, 400)
+            } else {
+                return baseResponse(res, null, "Error creating user", undefined, result.status, 500);
+            }
         }
+        return baseResponse(res, result.data, "User created.");
     }
 }
