a22b8e8882aa48daf9a0df48af73e58b21d5f729
AmirHosein <awir.software@gmail.com>
2023-04-12 16:58:25 +0430

    add base response helper function

---
+++ src/Helper/BaseResponse.ts
@@ -0,0 +1,29 @@
+export default function baseResponse(res: any, data: any = {}, message: string = "", token: any = undefined, status: string = "ok", statusCode = 200) {
+
+    if (typeof res === 'undefined') {
+        return new Error('response object is not set')
+    }
+
+    let response = {}
+    if (typeof token === 'undefined') {
+
+        response = {
+            status,
+            message,
+            data,
+        }
+
+        res.status(statusCode).send(response)
+        return
+    }
+
+    response = {
+        status,
+        message,
+        data,
+        token
+    }
+
+    res.status(statusCode).send(response)
+
+}
