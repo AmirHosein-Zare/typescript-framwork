cf5e75a6e09ce942def174f317ea4b17f41ffbfd
AmirHosein <awir.software@gmail.com>
2023-04-19 17:55:51 +0430

    add morgan in middleware

--- src/Middleware/Middleware.ts
+++ src/Middleware/Middleware.ts
@@ -1,8 +1,10 @@
 import { Express, json, urlencoded } from "express";
+import morgan from "morgan";
 
 export default class Middleware{
     init(app: Express){
         app.use(json());
         app.use(urlencoded({extended: true}));
+        app.use(morgan('tiny'));
     }
 }
