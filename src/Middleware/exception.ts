252afdc04a7c42287db3b875ced542a2da0e5cfa
AmirHosein <awir.software@gmail.com>
2023-04-19 18:01:28 +0430

    add exception middleware

--- src/Middleware/exception.ts
+++ src/Middleware/exception.ts
@@ -1,3 +1,7 @@
-export default function exception(){
-    
+import { NextFunction, Request, Response } from "express";
+import { error } from "winston";
+
+export default function exception(err: Error ,req: Request, res: Response, next: NextFunction){
+    error(err.message, err);
+    res.status(500).send('server error');
 }
