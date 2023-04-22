6b58631a1e5dabddb1e020dffb31273b463cd014
AmirHosein <awir.software@gmail.com>
2023-04-22 15:52:57 +0430

    install express-async-errors for logger service

---
+++ src/Utils/Logger/Logger.ts
@@ -0,0 +1,27 @@
+const config = require('config');
+const winston = require('winston');
+// require('winston-mongodb');
+require('express-async-errors');
+
+export default function logger(){
+    winston.add(
+        new winston.transports.File({filename:'logfile.log'})
+    ); 
+    // winston.add(
+    //     new winston.transports.MongoDB({db: config.get('db')})
+    // );
+    // winston.handleExceptions(
+    //     new winston.transports.Console({prettyprint: true}),
+    //     new winston.transports.File({ filename: 'unhandleException.log' })
+    // );
+
+    winston.exceptions.handle(new winston.transports.File({ filename: 'rejection.log' }));
+
+    process.on('uncaughtException', (ex)=> {
+        winston.error(ex);
+    })
+
+    process.on('unhandledRejection', (ex) => {
+        winston.error(ex);
+    })
+}
